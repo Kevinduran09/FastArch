3b618de67fd55bb969d833b948f4fd25eb7288bf
Kevin Andrey Durán Martínez <126124772+Kevinduran09@users.noreply.github.com>
2026-07-02 22:56:06 -0600

    Keep Arches container internal

--- fastarch/arches/__init__.py
+++ fastarch/arches/__init__.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from fastarch.arches.arch import Arch
 from fastarch.arches.binding import Binding, BindingBuilder, bind
-from fastarch.arches.container import Container
 from fastarch.arches.scopes import Scope
 from fastarch.arches.wiring import wires
 
@@ -12,7 +11,6 @@ __all__ = [
     "Arch",
     "Binding",
     "BindingBuilder",
-    "Container",
     "Scope",
     "bind",
     "wires",
